f7a6096ba8052b49f69cb32a07bf427c9e4e0b58
ohnoxt <noxt16@mail.ru>
2019-02-01 23:45:38 +0600

    task 4 2d arrays

---
+++ w1/task4/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class lookatthisdude
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); //creating size of array
+            int[,] r = new int[n, n]; //array is 2D so it has n rows and n columns
+            for (int i = 0; i < n; i++) //made for [i] elements(rows)
+            {
+                for (int j = 0; j <= i; j++) //made for [j] elements, but because of the fact that columns bigger than rows for 1 element it reaches to the [i] element 
+                {
+                    Console.Write("[*]"); //==cout
+                }
+                Console.WriteLine(); //end of the line for the current line
+            }
+        }
+    }
+}
